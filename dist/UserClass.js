0a8a232c37638c0f3bcdc0aa6dfa2b1843b18ac0
IbrahimOraby <orabyibrahim19@gmail.com>
2025-05-06 19:10:24 +0300

    Fix private Id in UserClass

--- dist/UserClass.js
+++ dist/UserClass.js
@@ -1,22 +1,19 @@
 export class User {
     constructor(id, name, age) {
-        this._id = id;
+        this.id = id;
         this.name = name;
         this.age = age;
     }
-    get id() {
-        return this._id;
-    }
     getDetails() {
         return `User Details: 
-                id: ${this._id},
+                id: ${this.id},
                 name: ${this.name},
                 age: ${this.age}`;
     }
 }
 export class Admin extends User {
-    constructor(role, _id, name, age) {
-        super(_id, name, age);
+    constructor(role, id, name, age) {
+        super(id, name, age);
         this.role = role;
     }
     getDetails() {
--- src/UserClass.ts
+++ src/UserClass.ts
@@ -3,22 +3,20 @@ interface HasID {
 }
 
 export class User implements HasID {
-	private _id: number;
+	public id: number;
 	protected name: string;
-	age: number;
+	public age: number;
 	constructor(id: number, name: string, age: number) {
-		this._id = id;
+		this.id = id;
 		this.name = name;
 		this.age = age;
 	}
 
-	get id(): number {
-		return this._id;
-	}
+
 
 	getDetails(): string {
 		return `User Details: 
-                id: ${this._id},
+                id: ${this.id},
                 name: ${this.name},
                 age: ${this.age}`;
 	}
@@ -26,8 +24,8 @@ export class User implements HasID {
 
 export class Admin extends User {
 	public role: string;
-	constructor(role: string, _id: number, name: string, age: number) {
-		super(_id, name, age);
+	constructor(role: string, id: number, name: string, age: number) {
+		super(id, name, age);
 		this.role = role;
 	}
 	getDetails(): string {
